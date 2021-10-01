cca079fde3f552f307b9ca3c4ea22e555482d23a
Daniel <DDABAHC89@GMAIL.COM>
2021-10-01 13:12:18 -0500

    lab1.danieldabah

--- js/index.js
+++ js/index.js
@@ -1,7 +1,39 @@
-// Iteration 1: Names and Input
+console.log ("Im ready")
+let hacker1 = ("Panchito")
+console.log ("the driver name is", hacker1)
+let hacker2 = ("Javier")
+console.log ("The navigator's name is", hacker2)
 
 
-// Iteration 2: Conditionals
 
 
-// Iteration 3: Loops
+console.log(`${hacker1} ${hacker1.length}`);
+console.log(`${hacker2} ${hacker2.length}`);
+
+if(hacker1.length> hacker2.length)
+ {
+  console.log ("The driver has the longest name, it has", hacker1.length)
+ }else{
+  console.log ("It seems that the navigator has the longest name, it has", hacker2.length)
+
+
+}
+
+console.log (hacker1.split (""))
+console.log (hacker2.split (""))
+
+console.log  (hacker1.toUpperCase ()) 
+console.log (hacker2.toUpperCase ())
+
+const hacker1Reversed = ("otihcnap")
+const hacker2Reversed = ('reivaj')
+
+console.log (hacker1Reversed.toUpperCase () )
+console.log (hacker2Reversed.toUpperCase ())
+
+if (hacker1.localeCompare = -2, -1)
+{
+  console.log ("The driver's name goes first.")
+}else{
+console.log ("Yo, the navigator goes first definitely.")
+}
